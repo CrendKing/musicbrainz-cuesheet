55ed114eb27b2c86c1062030aaeaa9608daf0a15
Crend King <975235+CrendKing@users.noreply.github.com>
2024-01-22 15:59:36 -0800

    Replace PathBuf with Box<Path>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fmt::Write;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::time::Duration;
 
 use chrono::Datelike;
@@ -21,7 +21,7 @@ struct Args {
     cover_art: bool,
 
     #[clap(short = 'o', long)]
-    out_dir: PathBuf,
+    out_dir: Box<Path>,
 }
 
 fn join_artists(artists: &[ArtistCredit]) -> Box<str> {
